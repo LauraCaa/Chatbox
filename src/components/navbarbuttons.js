cb344c7f62a793504212b26e3aa9207d6c390cb6
Camila <cardenascastrolauracamila7@gmail.com>
2023-08-08 23:36:56 -0500

    feat: first buttons in the app

---
+++ src/components/navbarbuttons.js
@@ -0,0 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from '../assets/images/Logo.png'
+
+export default function NavbarButtons() {
+    return(
+        <div class="container-fluid">
+            <div class="row">
+                <div>
+                    <img src={Logo} />
+                </div>
+                <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                    <div class="position-sticky">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <button class="nav-link btn btn-light">Dashboard</button>
+                            </li>   
+                            <li class="nav-item">
+                                <button class="nav-link btn btn-light">Analytics</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link btn btn-light">Files</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link btn btn-light">Call</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link btn btn-light">Messages</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link btn btn-light">Community</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link btn btn-light">Settings</button>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import NavbarButtons from "@/components/navbarbuttons"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function Home() {
+  return (
+    <div>
+       <div class="row">
+        <div class="col-2">
+          <NavbarButtons></NavbarButtons>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-6"></div>
+       </div>
+        
+    </div>
+  )
+}
